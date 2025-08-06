e506fe92d17019197ef9e81bb79f4a92f7ad366a
11056012 <05162006keke@gmail.com>
2025-08-06 18:25:46 +0800

    url更新下單流程

--- goodBuy_order/urls/order_action.py
+++ goodBuy_order/urls/order_action.py
@@ -2,7 +2,8 @@ from django.urls import path
 from goodBuy_order.views import *
 
 urlpatterns = [
-    path('checkout/', checkout, name='checkout'),
+    path('checkout/', checkout_step1, name='checkout'),
+    path('checkout/address_payment', checkout_step2, name='checkout_address_payment'),
     path('buyer_action/<int:order_id>/', buyer_action, name='buyer_action'),
     path('seller_action/<int:order_id>/', seller_action, name='seller_action'),
     path('batch_seller_action/', batch_seller_action, name='batch_seller_action'),
