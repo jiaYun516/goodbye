4c83165c0c47e1f548306ffa111920b677436889
Your Name <11056039@ntub.edu.tw>
2025-09-03 20:03:25 +0800

    側邊欄顯示標籤 漢堡選單小改
    有改了setting

--- goodBuy/settings.py
+++ goodBuy/settings.py
@@ -80,6 +80,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'goodBuy_tag.context_processors.user_followed_tags',#標籤顯示在每個主頁
             ],
         },
     },
---
+++ goodBuy_tag/context_processors.py
@@ -0,0 +1,12 @@
+#側邊欄顯示標籤
+from .models import Tag, TagCollect
+
+def user_followed_tags(request):
+    if not request.user.is_authenticated:
+        return {'user_followed_tags': []}
+
+    tag_ids = TagCollect.objects.filter(user=request.user)\
+                                .values_list('tag_id', flat=True)
+    tags = Tag.objects.filter(id__in=tag_ids).order_by('name')\
+                      .only('id', 'name')  
+    return {'user_followed_tags': tags}
