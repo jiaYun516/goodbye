8247dd5d8e898c08a9b2529530ebfb92f5b67ba1
L1zn <prkwh0629@gmail.com>
2025-09-13 22:58:58 +0800

    更改url條件

--- goodBuy/urls.py
+++ goodBuy/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
+
 from goodBuy_web.views.user_login_register import *
 from goodBuy_web.views import *
 from goodBuy_shop.views import my_shops_collected
@@ -37,3 +39,7 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    urlpatterns +=[
+        re_path(r'^upload/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    ]
